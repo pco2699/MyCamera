a5d45a3793f222f200649ef52bd2482167009472
pco2699 <aenkun@gmail.com>
2017-06-10 12:19:21 +0900

    Camera Function Completed

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,7 +19,51 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  @IBOutlet weak var pictureImage: UIImageView!
+  @IBAction func cameraButtonAction(_ sender: Any) {
+    // カメラが利用可能かチェック
+    if UIImagePickerController.isSourceTypeAvailable(.camera){
+      print("カメラは利用できます")
+      // (1) UIImagePickerControllerのインスタンスを作成
+      let ipc = UIImagePickerController()
+      // (2) sourceTypeにCameraを設定
+      ipc.sourceType = .camera
+      // (3)delegate設置
+      ipc.delegate = self
+      // (4)モーダルビューで表示
+      present(ipc, animated: true, completion: nil)
+    }
+    else {
+      print("カメラは利用できません")
+    }
+  }
 
+  @IBAction func SNSButtonAction(_ sender: Any) {
+    // 表示画像をアンラップしてシェア画像として取り出し
+    if let shareImage = pictureImage.image {
+      // UIActivityViewControllerを渡す配列を作成
+      let shareItems = [shareImage]
+      
+      // UIActivityViewContollerにシェア画像を渡す
+      let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+      
+      // iPadで落ちてしまう対策
+      controller.popoverPresentationController?.sourceView = view
+      
+      // UIActivityViewContollerを表示
+      present(controller, animated: true, completion: nil)
+      
+      
+    }
+  }
+  
+  // (1)撮影が終わったときに呼ばれるdelegateメソッド
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    // (2)撮影した写真を、配置したpictureImageを渡す
+    pictureImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    // (3)モーダルビューを閉じる
+    dismiss(animated: true, completion: nil)
+  }
 
 }
 
